0b6bc85ea301d78ab1110e5e4a877e66fa47e9a0
Patrick M. Menzel <pmmenzel@gmail.com>
2015-02-25 16:07:16 -0800

    change routes to scope specific user actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  root to: "users#index"
+
+  resources :users, except: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
