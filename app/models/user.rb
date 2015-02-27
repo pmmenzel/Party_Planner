ed6f08ffa6185770181b6ec9195391999ec08f06
Frank Shaw <fhshaw7@gmail.com>
2015-02-26 17:40:52 -0800

    clean up commented out ex code

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,3 @@ class User < ActiveRecord::Base
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
-
-
-
-# class Picture < ActiveRecord::Base
-#   belongs_to :imageable, polymorphic: true
-# end
-
-# class Employee < ActiveRecord::Base
-#   has_many :pictures, as: :imageable
-# end
-
-# class Product < ActiveRecord::Base
-#   has_many :pictures, as: :imageable
-# end
