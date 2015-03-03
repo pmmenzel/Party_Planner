7dcdd83b8cba53fffbd42180af10c2acea3bfd65
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-02 17:40:04 -0800

    create basic tests for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Event do
+		it { should belong_to :user}
+		it { should belong_to :guestlist}
+		it { should have_many :items}
+	end
+
+	describe "Event Count" do
+		before(:each) do
+			Event.destroy_all
+		end
+
+		context "Empty Events Table " do
+			it "should not have any Events" do
+				events =  Event.all
+				expect(events.length).to eq(0)
+			end
+		end
+
+		context "New Event" do
+			it "should create Event record" do
+				events =  Event.all
+				event = Event.create()
+				expect(events.length).to eq(1)
+			end
+		end
+	end
+
