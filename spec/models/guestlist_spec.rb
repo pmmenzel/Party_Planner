9bb6876e9aa1186bf1520a9f4a999c5a27b3c609
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-02 17:42:31 -0800

    create basic tests for Guestlist model

--- spec/models/guestlist_spec.rb
+++ spec/models/guestlist_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Guestlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Guestlist do
+		it { should have_one :event}
+		it { should have_many :users}
+		it { should_not have_many :items}
+	end
+
+	describe "Guestlist Count" do
+		before(:each) do
+			Guestlist.destroy_all
+		end
+
+		context "Empty Guestlists Table " do
+			it "should not have any Guestlists" do
+				guestlists =  Guestlist.all
+				expect(guestlists.length).to eq(0)
+			end
+		end
+
+		context "New Guestlist" do
+			it "should create Guestlist record" do
+				guestlists =  Guestlist.all
+				guestlist = Guestlist.create()
+				expect(guestlists.length).to eq(1)
+			end
+		end
+	end
