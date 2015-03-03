4a7e1afe55def3f85b531a2769f7b0a8ac195a21
bjjmeche <fhshaw7@gmail.com>
2015-03-02 17:59:11 -0800

    fix indentation error

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 describe Event do
-		it { should belong_to :user}
-		it { should belong_to :guestlist}
-		it { should have_many :items}
+	it { should belong_to :user}
+	it { should belong_to :guestlist}
+	it { should have_many :items}
+end
+
+describe "Event Count" do
+	before(:each) do
+		Event.destroy_all
 	end
 
-	describe "Event Count" do
-		before(:each) do
-			Event.destroy_all
-		end
-
-		context "Empty Events Table " do
-			it "should not have any Events" do
-				events =  Event.all
-				expect(events.length).to eq(0)
-			end
-		end
-
-		context "New Event" do
-			it "should create Event record" do
-				events =  Event.all
-				event = Event.create()
-				expect(events.length).to eq(1)
-			end
+	context "Empty Events Table " do
+		it "should not have any Events" do
+			events =  Event.all
+			expect(events.length).to eq(0)
 		end
 	end
+
+	context "New Event" do
+		it "should create Event record" do
+			events =  Event.all
+			event = Event.create()
+			expect(events.length).to eq(1)
+		end
+	end
+end
 
