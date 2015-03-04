f2df05c29fbe5e050ff7689ec4e8dfbf5d7012fb
bjjmeche <fhshaw7@gmail.com>
2015-03-04 13:10:50 -0800

    add Event controller

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,39 @@
+class EventController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params.merge(user_id: current_user.id))
+    if @event.save
+      redirect_to event_path(@event)
+    end
+  end
+
+  def show
+    @event =  Event.find_by_user_id(params[:user_id])
+  end
+
+  def edit
+    @event =  Event.find_by_user_id(params[:user_id])
+  end
+
+  def destroy
+    @event =  Event.find_by_user_id(params[:user_id])
+    user =  params[:user_id]
+    # user = User.find(current_user.id)
+    @event.destroy
+    redirect_to user_path(user)
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :public_party, :user_id, :guestlist_id)
+  end
+
+end
+
+
