c3b5eb1089712e15a4cb789a51b6f7a6ce2a3cb4
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-05 10:13:56 -0800

    add validations to Item model and add min number for quanity in the new item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :event
+
+  validate :name, :quantity, :event_id, :presence => true
+
+  validates_numericality_of :quantity, :only_integer =>true,
+                            :greater_than_or_equal_to =>1,
+                            :message => "Can't be a negative number"
 end
