347ae24ea37f0fe87d9f0b921f58d386ac4db6a3
bjjmeche <fhshaw7@gmail.com>
2015-03-04 17:51:48 -0800

    use partial to render show guestlist instead of hitting ../guestlist route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,8 @@ class EventsController < ApplicationController
 
   def show
     @event =  Event.find(params[:id])
+    @guestlist = Guestlist.find_by_event_id(params[:event_id])
+
   end
 
   def edit
--- app/controllers/guestlist_controller.rb
+++ app/controllers/guestlist_controller.rb
@@ -1,7 +1,3 @@
 class GuestlistController < ApplicationController
 
-  def show_guestlist
-    @guestlist = Guestlist.find_by_event_id(params[:event_id])
-  end
-
 end
