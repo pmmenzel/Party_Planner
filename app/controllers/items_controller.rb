811c952c0e83d6f0ac74e1f4211cd76825414d28
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-04 21:01:12 -0800

    add logic for actions in items_controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,52 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.where(event_id: params[:event_id])
+    @event = Event.find(params[:event_id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    p "*"*50
+    p item_params
+    @item = Item.new(item_params.merge(event_id: params[:event_id]))
+    if @item.save
+      redirect_to user_event_items_path(@item.event.user, @item.event)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update_attributes(item_params)
+    if item.save
+      redirect_to user_event_items_path(item.event.user, item.event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id]).destroy
+    event = Event.find(params[:event_id])
+    redirect_to user_event_items_path(event.user, event)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :quantity, :description, :event_id)
+  end
+end
