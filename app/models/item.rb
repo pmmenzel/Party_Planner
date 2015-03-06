29e9bb150cbc7f53572ab4e06e842b41db876860
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-06 08:26:27 -0800

    change 'validate' to validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :event
 
-  validate :name, :quantity, :event_id, :presence => true
+  validates :name, :quantity, :event_id, :presence => true
 
   validates_numericality_of :quantity, :only_integer =>true,
                             :greater_than_or_equal_to =>1,
