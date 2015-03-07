fc80895797d6c8c3ddf9fe05e2ca6460f0ce6760
Patrick M. Menzel <pmmenzel@gmail.com>
2015-03-06 16:00:21 -0800

    update user_controller update action and create basic layout for user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,19 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def update
+ def update
+    user = current_user
+    user.update_attributes(user_params)
+    if user.save
+      redirect_to user_url(user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :type)
   end
 end
