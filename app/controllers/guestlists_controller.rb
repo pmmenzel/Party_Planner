9bb96515a953bd0c6dffcb13edfabb3ef2d00be6
bjjmeche <fhshaw7@gmail.com>
2015-03-20 17:07:14 -0700

    adding guestlists controller and associated actions

--- app/controllers/guestlists_controller.rb
+++ app/controllers/guestlists_controller.rb
@@ -1,6 +1,8 @@
 class GuestlistsController < ApplicationController
   def invite_list
-
+    event = Event.find(params[:event_id])
+    # @user = User.find(params[:user_id])
+    @guestlist = event.guestlist
   end
 
   def invite
