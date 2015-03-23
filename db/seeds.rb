6462579ade2256f6ef924ba3aeb66d9a6de1e44b
bjjmeche <fhshaw7@gmail.com>
2015-03-23 00:13:14 -0700

    assign guestlist event and save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
 10.times do
-  Event.create()
+
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+
+  guestlist = Guestlist.create(
+    )
+
+  event = Event.create(
+    name: Faker::Name.name,
+    date: Faker::Date.forward(30),
+    location: Faker::Address.street_address,
+    public_party: [true, false].sample,
+    user_id: user.id,
+    guestlist_id: guestlist.id
+    )
+
+  guestlist.event_id = event.id
+  guestlist.save
+
 end
