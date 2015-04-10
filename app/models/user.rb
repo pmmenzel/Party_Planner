e72101be87d40e1809aca940de973e5bd711efd9
bjjmeche <fhshaw7@gmail.com>
2015-04-10 16:59:43 -0700

    add devise confirmable methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
