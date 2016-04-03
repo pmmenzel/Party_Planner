c2740d6b31be5764c0afd236adbf0f5d8bc4e72f
Patrick M. Menzel <pmmenzel@gmail.com>
2016-04-03 09:15:30 -0700

    add user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,9 +7,29 @@ describe UsersController do
   end
 
   describe "#show" do
-    sign_in(user)
-    get :show, id: user.id
-    expect(status).to eq(200)
+    it 'will not allow you to view show page if not an authenticated user' do
+      get :show
+      expect_redirect_to(new_user_session_path)
+    end
+
+    it "will allow user to view the user show page" do
+      sign_in(user)
+      get :show, id: user.id
+      expect_success
+    end
+  end
+
+  describe "#edit" do
+    it 'will not allow you to view the user edit page if not an authenticated user' do
+      get :edit, id: user.id
+      expect_redirect_to(new_user_session_path)
+    end
+
+    it "will allow user to view the user show page" do
+      sign_in(user)
+      get :edit, id: user.id
+      expect_success
+    end
   end
 
 end
