ba33b7b3bd61a62f7e9e3c4bb73f804c61aab1da
Patrick M. Menzel <pmmenzel@gmail.com>
2017-09-23 17:07:46 -0400

    remove guest -- moving towards event direct relationship with users

--- db/migrate/20150226221038_create_guestlists.rb
+++
@@ -1,9 +0,0 @@
-class CreateGuestlists < ActiveRecord::Migration
-  def change
-    create_table :guestlists do |t|
-      t.integer :event_id
-
-      t.timestamps
-    end
-  end
-end
--- spec/controllers/guestlists_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-# RSpec.describe GuestlistsController, type: :controller do
-
-# end
